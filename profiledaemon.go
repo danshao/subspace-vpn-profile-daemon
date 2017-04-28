57108671df3567d9ec8727f78fa4fda7884fd0bd
Cateyes <lovelycateyes@gmail.com>
2017-04-28 11:41:48 +0800

    Import project

---
+++ profiledaemon.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/takama/daemon"
+
+	"gitlab.ecoworkinc.com/subspace/softetherlib/softether"
+	"gitlab.ecoworkinc.com/subspace/subspace-utility/subspace/repository"
+)
+
+const (
+	SOFTETHER_ENDPOINT = "localhost"
+	SOFTETHER_PASSWORD = "subspace"
+	HUB                = "subspace"
+	
+	MYSQL_ENDPOINT = "localhost"
+	MYSQL_ACCOUNT = "subspace"
+	MYSQL_PASSWORD = "subspace"
+	MYSQL_DATABASE = "subspace"
+)
+
+const (
+	// Name of the service
+	name        = "vpnuser"
+	description = "Snapshot VPN user historical data"
+)
+
+//	dependencies that are NOT required by the service, but might be used
+var dependencies = []string{
+	"vpnserver.service", // This daemon depends on soft-ether vpn server
+}
+
+var stdlog, errlog *log.Logger
+
+// Service has embedded daemon
+type Service struct {
+	daemon.Daemon
+}
+
+// Manage by daemon commands or run the daemon
+func (service *Service) Manage() (string, error) {
+
+	usage := "Usage: myservice install | remove | start | stop | status"
+
+	// if received any kind of command, do it
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		switch command {
+		case "install":
+			return service.Install()
+		case "remove":
+			return service.Remove()
+		case "start":
+			return service.Start()
+		case "stop":
+			return service.Stop()
+		case "status":
+			return service.Status()
+		default:
+			return usage, nil
+		}
+	}
+
+	// Do something, call your goroutines, etc
+	vpnServer := softether.SoftEther{
+		IP:       SOFTETHER_ENDPOINT,
+		Password: SOFTETHER_PASSWORD,
+		Hub:      HUB,
+	}
+	repo := repository.MysqlProfileRepository{
+		Host: MYSQL_ENDPOINT,
+		Account: MYSQL_ACCOUNT,
+		Password: MYSQL_PASSWORD,
+		DatabaseName: MYSQL_DATABASE,
+	}
+	runner := ProfileDaemonRunner{
+		Server:            vpnServer,
+		ProfileRepository: repo,
+	}
+	runner.Start()
+
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	// loop work cycle with accept connections or interrupt
+	// by system signal
+	for {
+		select {
+		case killSignal := <-interrupt:
+			stdlog.Println("Got signal:", killSignal)
+			runner.Stop()
+			if killSignal == os.Interrupt {
+				return "Daemon was interruped by system signal", nil
+			}
+			return "Daemon was killed", nil
+		}
+	}
+
+	// never happen, but need to complete code
+	return usage, nil
+}
+
+func init() {
+	stdlog = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	errlog = log.New(os.Stderr, "", log.Ldate|log.Ltime)
+}
+
+func main() {
+	srv, err := daemon.New(name, description, dependencies...)
+	if err != nil {
+		errlog.Println("Error: ", err)
+		os.Exit(1)
+	}
+	service := &Service{srv}
+	status, err := service.Manage()
+	if err != nil {
+		errlog.Println(status, "\nError: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(status)
+}
---
+++ runner.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"gitlab.ecoworkinc.com/subspace/softetherlib/softether"
+
+	"gitlab.ecoworkinc.com/subspace/subspace-utility/subspace/model"
+	"gitlab.ecoworkinc.com/subspace/subspace-utility/subspace/repository"
+	"gitlab.ecoworkinc.com/subspace/subspace-utility/subspace/utils"
+)
+
+type ProfileDaemonRunner struct {
+	Server            softether.SoftEther
+	ProfileRepository repository.MysqlProfileRepository
+}
+
+const INTERVAL = 2 * time.Second
+const REDIS_TIME_TO_LIVE = 30 * time.Second
+
+var ticker *time.Ticker
+
+func (daemon ProfileDaemonRunner) Start() {
+	ticker = time.NewTicker(INTERVAL)
+
+	go func() {
+		for t := range ticker.C {
+
+			fmt.Println("Start fetch subspace status at", time.Now())
+			userList, code := daemon.Server.GetUserList()
+
+			if 0 != code {
+				fmt.Println(t, "Get UserList fail code:", code)
+				// Pending if SoftEther is too busy
+				daemon.pending()
+				continue
+			}
+
+			fmt.Println("User List Start")
+			profileSnapshots := make([]*model.ProfileSnapshot, 0)
+			for _, rawData := range userList {
+				userName := rawData["User Name"]
+
+				fmt.Println("User Get ", userName)
+				if userDetail, code := daemon.Server.GetUserInfo(userName); 0 == code {
+					if profile, err := utils.ParseUserGet(daemon.Server.Hub, userDetail); nil == err {
+						profileSnapshots = append(profileSnapshots, profile)
+					} else {
+						fmt.Println(t, "UserGet format is not expected")
+					}
+				} else {
+					fmt.Println(t, "Get UserGet fail code:", code)
+				}
+			}
+
+			if err := daemon.ProfileRepository.InsertBatch(profileSnapshots); nil != err {
+				fmt.Println(err)
+			}
+		}
+	}()
+}
+
+func (daemon ProfileDaemonRunner) Stop() {
+	ticker.Stop()
+}
+
+func (daemon ProfileDaemonRunner) pending() {
+	time.Sleep(REDIS_TIME_TO_LIVE / 5)
+}
